0ba432461829cb95ba878835f3af4f3eb411560b
RezaFahlevi28 <daartsf@gmail.com>
2020-01-29 13:36:25 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -11,6 +11,10 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum sunt voluptatum, aperiam voluptatibus id possimus, ab dicta, magni quo temporibus a fuga provident dolorem exercitationem laborum laudantium minus itaque consequatur?</p>
+    <ul>
+        <li>one</li>
+        <li>two</li>
+    </ul>
 </body>
 
 </html>
